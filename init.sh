95beb06165c03499f7e642ac691ae96527cf7675
Vassilyev Denis <tarapiygin@gmail.com>
2019-08-17 21:36:50 +0300

    fdfdf

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ﻿ln -sf /home/tarapiygin/box/web/etc/nginx.conf  /etc/nginx/conf.d/default.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/conf.d/default.conf
 sudo /etc/init.d/nginx restart
