470e665ff84aee08dfe10861602832f32532fcb4
Vassilyev Denis <tarapiygin@gmail.com>
2019-08-18 16:52:10 +0300

    1234

---
+++ myinit.sh
@@ -0,0 +1,5 @@
+ sudo ln -sf /home/tarapiygin/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+ sudo rm -rf /etc/nginx/sites-enabled/default
+ sudo /etc/init.d/nginx restart
+ sudo ln -sf /home/tarapiygin/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+ sudo /etc/init.d/gunicorn restart
