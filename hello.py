1db9a091316db9fa704c099718070c73e06236a5
Vassilyev Denis <tarapiygin@gmail.com>
2019-08-18 15:38:08 +0300

    hello.py - wsgi app

---
+++ hello.py
@@ -0,0 +1,6 @@
+def wsgi_app(environ, start_response):
+	status = '200 OK'
+	headers = ['Content-type','text-plain']
+	body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+	start_response(status, headers)
+	return body
