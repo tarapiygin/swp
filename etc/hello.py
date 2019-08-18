c2f05dff30729f216f95db34e5dee659b873a69f
Vassilyev Denis <tarapiygin@gmail.com>
2019-08-18 18:02:08 +0300

    asdsad

--- etc/hello.py
+++ etc/hello.py
@@ -4,9 +4,6 @@ CONFIG = {
     'python': '/usr/bin/python3',
     'args': (
         '--bind=0.0.0.0:8080',
-        '--workers=16',
-        '--timeout=60',
-		'--log-level=debug',
         'hello:app',
     ),
 }
